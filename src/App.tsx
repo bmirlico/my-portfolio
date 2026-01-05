0c6135569cb7bb0404fa4eef01fb4b8c7bf17e57
bmirlico <bastienmirlicourtois@gmail.com>
2026-01-05 17:17:30 +0100

    added particles background on website

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Particles } from "@/components/ui/particles";
 import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
@@ -9,6 +10,12 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <Particles
+        className="fixed inset-0 -z-10"
+        quantity={80}
+        color="#0d9488"
+        ease={80}
+      />
       <Toaster />
       <Sonner />
       <Index />
